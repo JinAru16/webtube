4561e7c29c7bd29764111d7b143db9f29c1a9539
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-02-08 21:34:11 +0900

    array로 db연습

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import { see, edit , deleteVideo, upload} from "../controllers/videoController";
+import { watch, edit , deleteVideo, upload} from "../controllers/videoController";
 
 const videoRouter = express.Router();//비디오 보기, 비디오 올리기, 비디오 지우기 등등
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id(\\d+)", see);// (\\d+)는 :id에서 숫자만 따오는거임. :id자리에 무슨 글자를 타이핑 하던 숫자만 긁어줌. 숫자+문자 조합도 안됨. 숫자만 올때 :id로 인식함
+
+videoRouter.get("/:id(\\d+)", watch);// (\\d+)는 :id에서 숫자만 따오는거임. :id자리에 무슨 글자를 타이핑 하던 숫자만 긁어줌. 숫자+문자 조합도 안됨. 숫자만 올때 :id로 인식함
 videoRouter.get("/:id(\\d+)/edit", edit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo)
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", upload);
+
 
 export default videoRouter;
 
--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import videoRouter from "./routers/videoRouter";
 
 const app = express();
 const logger = morgan("dev");
-console.log(process.cwd());
+
 
 app.set("view engine", "pug"); // pug를 기본 뷰 엔진으로 설정. package.json이 있는곳이 cwd임. cwd = 현재 작업 디렉토리의 위치
 app.set("views", process.cwd() + "/src/views"); // package.json이 있는곳이 아닌 src폴더가 있는 views폴더로 디렉토리 위치를 변경해줌
