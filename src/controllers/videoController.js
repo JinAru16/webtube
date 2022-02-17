6abf861c4b0f5b0511347011aa7949938c5aca36
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-02-17 18:34:27 +0900

    video.save()를 사용해 실제 DB에 영상 객체가 저장되로록 설정

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,15 @@ import { type } from "express/lib/response";
 import Video from "../models/Video";
 
   
-export const home = (req, res) => {
-  Video.find({},(error, videos)) // ()속의 {}는 serch terms를 나타내는데, {}속이 비어있다면 모든 형식을 찾는다는 뜻.
-  return res.render("home", { pageTitle: "Home" });
+export const home = async(req, res) => {
+  try{
+  const videos = await Video.find({});// ()속의 {}는 serch terms를 나타내는데, {}속이 비어있다면 모든 형식을 찾는다는 뜻.
+  console.log(videos);
+  return res.render("home", { pageTitle: "Home", videos })
+  
+} catch(error) {
+    console.log(error)
+  }
 };
 export const watch = (req, res) => {
   const { id } = req.params;
@@ -24,11 +30,34 @@ export const postEdit = (req, res) => {
 
 export const getUpload = (req, res) => {
   return res.render("upload", {pageTitle: "Upload Video"});
-}
+};
 
-export const postUpload = (req, res) => {
+export const postUpload = async(req, res) => {
   // here we will add video to the videos array.
-  const { title } = req.body
-  videos.push(newVideo)
-  return res.redirect(`videos/${newVideo.id}`);
+  const { title, description, hashtags } = req.body;
+  try {
+  const video = new Video({
+    title: title, // 왼쪽 title은 스키마의 title을 나타내고 오른쪽 title은 req.body를 뜻함. 
+    description: description,
+    hashtags: hashtags.split(" ").map(word => `#${word}`),
+
+    /* create방식도 있음.
+    await Video.create({
+      title,
+      diescription,
+      .
+      .
+
+    })
+    video.save()는 안쓰는거임. 그냥 create로  끝남
+    */
+  });
+  await video.save();
+  return res.redirect("/");
+} catch(error) {
+  console.log(error)
+  return res.render("upload", { pageTitle: "Upload Video", 
+  errorMessage: error._message,
+ });
 }
+};
--- src/models/Video.js
+++ src/models/Video.js
@@ -1,13 +1,13 @@
 import  Mongoose  from "mongoose";
 
 const videoSchema = new Mongoose.Schema({
-    title: String,
-    description: String,
-    createdAt: Date,
-    hashtags: [{type: String}],
+    title: { type: String, required: true, trim: true, maxlength: 80 },
+    description: { type: String, required: true, trim: true, minlength: 200 },
+    createdAt: { type: Date, required: true, default: Date.now }, //now에 ()가 안붙는 이유는 바로 함수를 바로 실행하게 두고 싶지 않아서. Video가 실행될때만 작동하게 만들기 위해 ()를 제거.
+    hashtags: [{type: String, trim: true }],
     meta:{
-        views: Number,
-        rating:  Number,
+        views: { type: Number, default: 0, required: true },
+        rating:  { type: Number, default: 0, required: true },
     },
 });
 
